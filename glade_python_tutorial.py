1eccd9e99ca8cf34c77740d7521f7918e338afe7
alfan-ntu <maoyi_fan@yahoo.com.tw>
2019-08-13 22:25:17 +0800

    Added an about dialog and status bar, also demonstrated additions of actions

--- glade_python_tutorial.py
+++ glade_python_tutorial.py
@@ -12,12 +12,40 @@ class Buglump:
         print("quit from menu")
         Gtk.main_quit()
 
+    # defined actions of menu items
+    def on_push_status_activate(self, menuitem, data=None):
+        self.status_count += 1
+        self.statusbar.push(self.context_id, "Message number %s" % str(self.status_count))
+
+    def on_pop_status_activate(self, menuitem, data=None):
+        self.status_count -= 1
+        self.statusbar.pop(self.context_id)
+
+    def on_clear_status_activate(self, menuitem, data=None):
+        while (self.status_count > 0):
+            self.statusbar.pop(self.context_id)
+            self.status_count -= 1
+
+    # added an activatable methon connected to Help->About
+    # connection is built in glade file using the following statement
+    # <signal name="activate" handler="on_gtk_about_activate" swapped="no"/>
+    def on_gtk_about_activate(self, menuitem, data=None):
+        print("Help about selected")
+        self.response = self.aboutdialog.run()
+        self.aboutdialog.hide()
+
     def __init__(self):
-        self.gladefile = "tutorial-1.glade"
+        self.gladefile = "glade_python_tutorial.glade"
         self.builder = Gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("window1")
+        # added a new about dialog
+        self.aboutdialog = self.builder.get_object("aboutdialog1")
+        # added status bar
+        self.statusbar = self.builder.get_object("statusbar1")
+        self.context_id = self.statusbar.get_context_id("status")
+        self.status_count = 0
         self.window.show()
 
 
