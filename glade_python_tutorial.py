3b919edeaf0c25c07e64894e09a78296c6435e20
alfan-ntu <maoyi_fan@yahoo.com.tw>
2019-08-13 12:40:51 +0800

    First check-in with a simple python and glade pair

---
+++ glade_python_tutorial.py
@@ -0,0 +1,26 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class Buglump:
+    def on_window1_destroy(self, object, data=None):
+        print("quit with cancel")
+        Gtk.main_quit()
+
+    def on_gtk_quit_activate(self, menuitem, data=None):
+        print("quit from menu")
+        Gtk.main_quit()
+
+    def __init__(self):
+        self.gladefile = "tutorial-1.glade"
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(self.gladefile)
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("window1")
+        self.window.show()
+
+
+if __name__ == "__main__":
+    main = Buglump()
+    Gtk.main()
