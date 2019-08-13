c3a67f71a84933d9678893557cda8824f9c9cbcd
alfan-ntu <maoyi_fan@yahoo.com.tw>
2019-08-13 23:18:49 +0800

    Added some labels and entries for doing specified calculation

--- glade_python_tutorial.py
+++ glade_python_tutorial.py
@@ -2,6 +2,11 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+try:
+    import math
+except:
+    print("math lib missing")
+    sys.exit(1)
 
 class Buglump:
     def on_window1_destroy(self, object, data=None):
@@ -33,6 +38,24 @@ class Buglump:
         print("Help about selected")
         self.response = self.aboutdialog.run()
         self.aboutdialog.hide()
+
+    def on_sfm_button_clicked(self, button, data=None):
+        # get entry objects and label objects to get and set contents
+        self.entry1 = self.builder.get_object("entry1")
+        self.entry2 = self.builder.get_object("entry2")
+        self.result = self.builder.get_object("result")
+
+        # get the text from the GtkEntry widget and convert
+        # it to a float value for calculation
+        self.sfm = float(self.entry1.get_text())
+        self.diameter = float(self.entry2.get_text())
+
+        # calculate the result and convert it to an integer, and then convert
+        # to a string, set the text of the label
+        self.rpm = str(int(self.sfm * ((12/math.pi)/self.diameter)))
+        print("calculate rpm clicked")
+        self.result.set_text(self.rpm)
+
 
     def __init__(self):
         self.gladefile = "glade_python_tutorial.glade"
