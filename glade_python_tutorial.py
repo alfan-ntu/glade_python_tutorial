65f544e252c0b57a49e9976f321d2adb789b4a9f
alfan-ntu <maoyi_fan@yahoo.com.tw>
2019-08-16 16:39:01 +0800

    Added a notebook and demonstrated implementation of a menu handler

--- glade_python_tutorial.py
+++ glade_python_tutorial.py
@@ -26,9 +26,34 @@ class Buglump:
             self.statusbar.pop(self.context_id)
             self.status_count -= 1
 
-    # added an activatable methon connected to Help->About
-    # connection is built in glade file using the following statement
-    # <signal name="activate" handler="on_gtk_about_activate" swapped="no"/>
+    # Menu File->New activate handler; adding a new tab using append_page method
+    def on_gtk_new_activate(self, menuitem, data=None):
+        # debugging Message
+        print("File New Selected")
+
+        # create a label for the tab and using get_n_pages() to find out how
+        # many pages there are so the next page has a sequential number.
+        self.label1 = Gtk.Label("Page " + str(self.notebook.get_n_pages() + 1))
+
+        # create a label to put the into the page
+        self.label2 = Gtk.Label("Hello world")
+        # label2 will not be displayed unitl .show() method is called
+        self.label2.show()
+        # append a new page with label5 as the contents and label5 as the tab
+        self.notebook.append_page(self.label2, self.label1)
+
+    def on_notebook1_switch_page(self, notebook, page, page_num, data=None):
+        # switch_page(notebook'object which received the signal,
+        #             page'the current page',
+        #             page_num'the index of the page')
+
+        # added an activatable methon connected to Help->About
+        # connection is built in glade file using the following statement
+        # signal name="activate" handler="on_gtk_about_activate" swapped="no"
+        self.tab = notebook.get_nth_page(page_num)
+        self.label = notebook.get_tab_label(self.tab).get_label()
+        self.message_id = self.statusbar.push(0, self.label)
+
     def on_gtk_about_activate(self, menuitem, data=None):
         print("Help about selected")
         self.response = self.aboutdialog.run()
@@ -46,6 +71,10 @@ class Buglump:
         self.statusbar = self.builder.get_object("statusbar1")
         self.context_id = self.statusbar.get_context_id("status")
         self.status_count = 0
+        # initialized a notebook
+        self.notebook = self.builder.get_object("notebook1")
+        self.on_notebook1_switch_page(self.notebook, '', 0)
+
         self.window.show()
 
         # suppose this if a change from branch gtk_note
