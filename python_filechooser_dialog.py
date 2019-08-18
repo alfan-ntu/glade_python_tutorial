e04e49cf6ac5a56e96e99a952a897db4fe0761dd
alfan-ntu <maoyi_fan@yahoo.com.tw>
2019-08-18 20:50:20 +0800

    Adjusted dialog window size and added file filter to the chooser dialog

--- python_filechooser_dialog.py
+++ python_filechooser_dialog.py
@@ -37,10 +37,35 @@ class Buglump():
                    action=Gtk.FileChooserAction.OPEN)
         self.fcd.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
         self.fcd.add_button(Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+        self.fcd.set_default_size(300, 200)
+        self.create_filechooser_filter()
         self.response = self.fcd.run()
         if self.response == Gtk.ResponseType.OK:
             print("Selected filepath: %s" % self.fcd.get_filename())
             self.fcd.destroy()
+
+    # added a file filter to the file chooser dialog
+    def create_filechooser_filter(self):
+        pdf_filter = Gtk.FileFilter()
+        pdf_filter.add_mime_type("application/pdf")
+        pdf_filter.set_name("*.pdf 檔案")
+        self.fcd.add_filter(pdf_filter)
+
+        txt_filter = Gtk.FileFilter()
+        txt_filter.add_mime_type("text/plain")
+        txt_filter.set_name("*.txt 文字檔案")
+        self.fcd.add_filter(txt_filter)
+
+        excel_filter = Gtk.FileFilter()
+        excel_filter.add_mime_type("application/vnd.ms-excel")
+        excel_filter.add_mime_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        excel_filter.set_name("*.xlsx Excel 檔案")
+        self.fcd.add_filter(excel_filter)
+
+        any_filter = Gtk.FileFilter()
+        any_filter.add_pattern("*.*")
+        any_filter.set_name("所有檔案")
+        self.fcd.add_filter(any_filter)
 
     def on_file_save_activate(self, menuitem, data=None):
         pass
@@ -49,7 +74,8 @@ class Buglump():
         pass
 
     def on_file_quit_activate(self, menuitem, data=None):
-        pass
+        print("Quit with cancel")
+        Gtk.main_quit()
 
     def on_window1_destroy(self, object, data=None):
         print("Quit with cancel")
